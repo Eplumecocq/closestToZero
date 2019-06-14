c108dbf186d7571c912d097ffc7e47c24e975c4f
Eliot Plumecocq <eliot.plumecoq@richdistrib.com>
2019-06-14 12:12:49 +0200

    add more tests

--- index.test.js
+++ index.test.js
@@ -1,7 +1,21 @@
 const closestToZero = require('./index');
 
-describe('test closestToZero function', () => {
-  it.each`
+describe('test closestToZero function with basic exemple tests', () => {
+  test('when input is [8, 5, 10] it must returns 5', () => {
+    expect(closestToZero([8, 5, 10])).toEqual(5);
+  });
+
+  test('when input is [5, 4, -9, 6, -10, -1, 8] it must returns -1', () => {
+    expect(closestToZero([5, 4, -9, 6, -10, -1, 8])).toEqual(-1);
+  });
+
+  test('when input is [8, 2, 3, -2] it must returns 2', () => {
+    expect(closestToZero([8, 2, 3, -2])).toEqual(2);
+  });
+});
+
+describe('test closestToZero function with advanced tests', () => {
+  test.each`
     a            | b            | result
     ${-1}        | ${1}         | ${1}
     ${-2}        | ${-3}        | ${-2}
